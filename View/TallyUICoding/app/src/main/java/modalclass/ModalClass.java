e3fbfacb8b9bf3e10d6172054f7704a9a9f379a4
hefl <flidea@outlook.com>
2019-10-22 15:32:29 +0800

    上传界面Login

---
+++ View/TallyUICoding/app/src/main/java/modalclass/ModalClass.java
@@ -0,0 +1,18 @@
+package modalclass;
+
+public class ModalClass {
+
+    String name;
+
+    public ModalClass(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
